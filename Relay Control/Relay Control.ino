2f0c446849cab2399cc4cc4444cc13e466abd5b6
Comyar Dehlavi <comyar2007@gmail,com>
2021-04-23 20:25:51 -0500

    Added sequencing for solonoid

--- Relay Control/Relay Control.ino
+++ Relay Control/Relay Control.ino
@@ -6,23 +6,23 @@
 #define relay_3 6
 
 bool state = false;
+int sequence_index = 0;
 
-bool relay_1_state = false;
-bool relay_2_state = false;
-bool relay_3_state = false;
-
-int interval = 600000;
-int previousTime = 0;
+const unsigned long long int interval = 10 * 60000;
+unsigned long previousTime = 0;
 
 void InterruptServiceRoutine_ON(){
   state = true;
+  sequence_index = 0;
 }
 
 void InterruptServiceRoutine_OFF(){
    for(int i = 4; i<= 6; i++){
     digitalWrite(i, LOW);
   }
+
   state = false;
+  sequence_index = 0;
 }
 
 void setup() {
@@ -40,13 +40,37 @@ void setup() {
 
 void loop() {
   if (state == true){
-    int currentTime = millis();
-
-    if(currentTime - previousTime >= interval){
-
+    unsigned long long currentTime = millis();
+    
+    if(currentTime - previousTime >= interval || sequence_index == 0){
+      switch(sequence_index){
+        case 0:
+          digitalWrite(relay_1, HIGH);
+          sequence_index = 1;
+          break;
+        case 1:
+          digitalWrite(relay_1, LOW);
+          digitalWrite(relay_2, HIGH);
+          sequence_index = 2;
+          break;
+        case 2:
+          digitalWrite(relay_2, LOW);
+          digitalWrite(relay_3, HIGH);
+          sequence_index = 3;
+          break;
+        case 3:
+          digitalWrite(relay_3, LOW);
+          sequence_index = 0;
+          state = false;
+          break;
+      }
       previousTime = currentTime;
     }
   } else{
-    previousTime = 0;
+     for(int i = 4; i<= 6; i++){
+      digitalWrite(i, LOW);
+     }
+      sequence_index = 0;
+      previousTime = 0;
   }
 }
